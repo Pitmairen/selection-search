5b32c4566bc9a3cbc94b86a1d1264183790b7e55
Pitmairen <progrper@gmail.com>
2013-03-03 16:57:51 +0100

    move postsearch script to external script

---
+++ postsearch.js
@@ -0,0 +1,59 @@
+
+$(document).ready(function (){
+
+
+	var query = document.location.search.substr(1).split('=');
+
+	if(query.length != 2){
+		$("#error").text('Search failed"');
+		return;
+	}
+
+	if(query[0] != 'url'){
+		$("#error").text('Search failed"');
+		return;
+	}
+
+
+	var url = decodeURIComponent(query[1]);
+// 	console.log(url);
+
+	var parts = url.split('{POSTENCODING}', 2);
+	var encoding = null;
+
+	if(parts.length == 2){
+		url = parts[0];
+		encoding = parts[1];
+	}
+
+	parts = url.split('{POSTARGS}', 2);
+
+	if(parts.length != 2){
+		$("#error").text('Invalid url for a POST search. The url must contain "{POSTARGS}"');
+		return;
+	}
+
+	var form = $('<form></form>')
+		.attr('method', 'post')
+		.attr('action', parts[0])
+
+	if(encoding){
+		form.attr('accept-charset', encoding);
+	}
+
+	var query = parts[1].split('&');
+
+	for(var i=0; i<query.length; ++i){
+		var key_value = query[i].split('=', 2);
+		if(key_value.length != 2)
+			continue;
+		form.append($('<input type="hidden" name="'+key_value[0]+'" value="'+key_value[1]+'" />'));
+	}
+
+
+// 	$("#error").text('Invalid url for a POST search. The url must contain "{POSTARGS}"');
+	form.submit();
+
+
+});
+
