608ea416dda3ab75ec5f188efa07e8c85742c962
Pitmairen <progrper@gmail.com>
2015-04-12 21:05:11 +0200

    Bump version

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ Storage.storage_upgrades();
 
 
 // Added in version 0.1.4
-localStorage['VERSION'] = '0.7.23';
+localStorage['VERSION'] = '0.7.24';
 
 // Id of the root menu when the contextmenu activator is used.
 var G_rootMenuId = null;
