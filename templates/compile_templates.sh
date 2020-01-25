41e97b06d5bee7fceae216e3805af7d911e71be2
Pitmairen <progrper@gmail.com>
2020-01-25 10:14:34 +0100

    Fix template compilation script

--- templates/compile_templates.sh
+++ templates/compile_templates.sh
@@ -1,7 +1,9 @@
 #!/usr/bin sh
 
-# doT templates is needed. (npm install dot)
+# doT templates is needed. (npm install)
 
-dottojs -s options -d _compiled -p options.js
+./node_modules/dot/bin/dot-packer -s options -d _compiled
 
-
+cat _compiled/folder_end.js > options.js
+cat _compiled/searchengine.js >> options.js
+cat _compiled/separator.js >> options.js
