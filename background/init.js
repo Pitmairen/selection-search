88e2e3951d92363c4214b3bf1fa45e35cfecc4fe
Pitmairen <progrper@gmail.com>
2020-04-28 20:25:33 +0200

    Bump version

--- background/init.js
+++ background/init.js
@@ -140,7 +140,7 @@ function initBackground(_previousVersion){
 
 (function(){
 
-    let CURRENT_VERSION = '0.8.55';
+    let CURRENT_VERSION = '0.8.56';
 
     storageLocalSyncInit(Storage).then(values => {
 
