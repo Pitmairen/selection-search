ec1e765a42ac1815f5a1b0778811b28939f5c1af
Pitmairen <progrper@gmail.com>
2024-12-30 11:24:45 +0100

    bump version

--- background/init.js
+++ background/init.js
@@ -148,7 +148,7 @@ function Background(_previousVersion) {
 
 function initBackground(){
 
-    let CURRENT_VERSION = '0.9.3';
+    let CURRENT_VERSION = '0.9.4';
 
     return storageLocalSyncInit(Storage).then(values => {
 
